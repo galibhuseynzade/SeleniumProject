49cdb8a4d39a090da6981d41a5402673e7b24899
galibhuseynzade <galibhuseynzade@gmail.com>
2025-09-11 17:51:49 +0400

    RadioButtonTest created

---
+++ src/test/java/RadioButtonTest.java
@@ -0,0 +1,52 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class RadioButtonTest {
+    @Test
+    public void radioButtonTest() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("disable-notifications");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+
+        driver.get("https://demoqa.com/");
+
+        String elements = "//div[@class='card-body']//h5[text()='Elements']";
+        WebElement elementsButton = driver.findElement(By.xpath(elements));
+        elementsButton.click();
+
+        String textBox = "//span[text()='Radio Button']";
+        WebElement textBoxButton = driver.findElement(By.xpath(textBox));
+        textBoxButton.click();
+
+        String impressiveRadio = "//label[@for='impressiveRadio']";
+        WebElement impressiveRadioButton = driver.findElement(By.xpath(impressiveRadio));
+        String impressiveRadioButtonClass = impressiveRadioButton.getAttribute("class");
+        Assertions.assertNotNull(impressiveRadioButtonClass);
+        if (impressiveRadioButtonClass.contains("disabled")) System.out.println("Impressive button is disabled");
+        else impressiveRadioButton.click();
+
+        String noRadio = "//label[@for='noRadio']";
+        WebElement noRadioButton = driver.findElement(By.xpath(noRadio));
+        String noRadioButtonClass = noRadioButton.getAttribute("class");
+        Assertions.assertNotNull(noRadioButtonClass);
+        if (noRadioButtonClass.contains("disabled")) System.out.println("No button is disabled");
+        else noRadioButton.click();
+
+        String result = "//span[@class='text-success']";
+        List<WebElement> resultList = driver.findElements(By.xpath(result));
+        List<String> resultText = resultList.stream().map(WebElement::getText).toList();
+        System.out.println(resultText);
+
+        driver.quit();
+    }
+}
